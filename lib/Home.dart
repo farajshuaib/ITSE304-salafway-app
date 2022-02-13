7fb60a81f0a0bda22edc6ccfa2a3cb4a74326066
faraj Shuaib <46637985+farajshuaib@users.noreply.github.com>
2022-02-13 13:34:57 +0200

    update the structure

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,7 +1,12 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
-import "./Streaming_controller.dart";
+import 'dart:async';
+import 'dart:io';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:http/http.dart' as http;
+
+typedef OnError = void Function(Exception exception);
+
+// https://qurani-api.herokuapp.com/api/reciters
 
 class Home extends StatefulWidget {
   Home({Key? key}) : super(key: key);
@@ -11,255 +16,32 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  var streamingController = StreamingController();
+  AudioPlayer player = AudioPlayer();
 
-  @override
-  void initState() {
+  late final dynamic data;
+
+  getData() async {
+    var url = Uri.parse('https://qurani-api.herokuapp.com/api/reciters');
+    var response = await http.get(url);
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+  }
+
+  initState() {
+    getData();
     super.initState();
-    streamingController.config(
-        url: "http://160.19.99.214:8000/fm"); // salfway fm
+  }
+
+  Widget remoteUrl() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [],
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Theme.of(context).primaryColor,
-      child: SafeArea(
-        child: Stack(
-          clipBehavior: Clip.none,
-          children: [
-            Container(
-              height: double.infinity,
-              width: double.infinity,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 60, 0, 0),
-                    child: Image(
-                      image: NetworkImage(
-                          'https://www.salafway.ly/assets/img/logo.png'),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            SizedBox.expand(
-              child: DraggableScrollableSheet(
-                initialChildSize: 0.3,
-                maxChildSize: 0.85,
-                minChildSize: 0.3,
-                builder:
-                    (BuildContext context, ScrollController scrollController) {
-                  return Stack(
-                    clipBehavior: Clip.none,
-                    alignment: Alignment.center,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(25.0),
-                            topRight: Radius.circular(25.0),
-                          ),
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: ListView(
-                            controller: scrollController,
-                            children: [
-                              SizedBox(
-                                height: 45,
-                              ),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    "Salafway-fm",
-                                    style: TextStyle(
-                                      fontSize: 24,
-                                      color: Color(0xff2D2E2F),
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Text(
-                                    "100.3",
-                                    style: TextStyle(
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.bold,
-                                      color: Color(0xff2D2E2F),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              const Divider(
-                                height: 25,
-                                thickness: 1,
-                                indent: 0,
-                                endIndent: 0,
-                              ),
-                              Text(
-                                "about salfway fm",
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                  letterSpacing: 1.3,
-                                  color: Color(0xff2D2E2F),
-                                ),
-                              ),
-                              SizedBox(
-                                height: 20,
-                              ),
-                              Text(
-                                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w400,
-                                  letterSpacing: 1.3,
-                                  color: Color(0xff2D2E2F),
-                                ),
-                              ),
-                              SizedBox(
-                                height: 20,
-                              ),
-                              Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Icon(
-                                    Icons.phone_android_outlined,
-                                    color: Theme.of(context).primaryColor,
-                                  ),
-                                  SizedBox(
-                                    width: 5,
-                                  ),
-                                  Text(
-                                    "00218911234567",
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      color: Color(0xff2D2E2F),
-                                      fontWeight: FontWeight.w400,
-                                    ),
-                                  )
-                                ],
-                              ),
-                              SizedBox(
-                                height: 20,
-                              ),
-                              Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Icon(
-                                    Icons.mail_outline,
-                                    color: Theme.of(context).primaryColor,
-                                  ),
-                                  SizedBox(
-                                    width: 5,
-                                  ),
-                                  Text(
-                                    "info@bla.com",
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      color: Color(0xff2D2E2F),
-                                      fontWeight: FontWeight.w400,
-                                    ),
-                                  )
-                                ],
-                              ),
-                              SizedBox(
-                                height: 20,
-                              ),
-                              Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Icon(
-                                    Icons.pin_drop_outlined,
-                                    color: Theme.of(context).primaryColor,
-                                  ),
-                                  SizedBox(
-                                    width: 5,
-                                  ),
-                                  Text(
-                                    "tripoli - libya",
-                                    style: TextStyle(
-                                      fontSize: 16,
-                                      color: Color(0xff2D2E2F),
-                                      fontWeight: FontWeight.w400,
-                                    ),
-                                  )
-                                ],
-                              ),
-                              const Divider(
-                                height: 25,
-                                thickness: 1,
-                                indent: 0,
-                                endIndent: 0,
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        top: -45.0,
-                        child: Container(
-                          clipBehavior: Clip.hardEdge,
-                          height: 90,
-                          width: 90,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Color(0xff2D2E2F),
-                            border: Border.all(
-                                width: 5,
-                                color: Theme.of(context).primaryColor),
-                          ),
-                          child: StreamBuilder(
-                            stream:
-                                streamingController.streamingController.stream,
-                            builder: (BuildContext context, snapshot) {
-                              if (snapshot.data == null ||
-                                  snapshot.data == "paused_event") {
-                                return IconButton(
-                                  icon: Icon(
-                                    Icons.play_arrow,
-                                    size: 44,
-                                    color: Colors.white,
-                                  ),
-                                  onPressed: () {
-                                    streamingController.play();
-                                  },
-                                );
-                              } else if (snapshot.data == "playing_event") {
-                                return IconButton(
-                                  icon: Icon(
-                                    Icons.pause,
-                                    size: 44,
-                                    color: Colors.white,
-                                  ),
-                                  onPressed: () {
-                                    streamingController.pause();
-                                  },
-                                );
-                              } else {
-                                return Padding(
-                                  padding: const EdgeInsets.all(15.0),
-                                  child: CircularProgressIndicator(
-                                    color: Theme.of(context).primaryColor,
-                                  ),
-                                );
-                              }
-                            },
-                          ),
-                        ),
-                      ),
-                    ],
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    return Container();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,120 @@
-import 'package:flutter/material.dart';
-import "./Home.dart";
+import 'dart:async';
 
-// https://qurani-api.herokuapp.com/api/reciters
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+import "./Home.dart";
+import './live_radio.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ConnectivityResult? _connectivityResult;
+  late StreamSubscription _connectivitySubscription;
+  int _selectedIndex = 0;
+
+  Future<void> _checkConnectivityState() async {
+    final ConnectivityResult result = await Connectivity().checkConnectivity();
+
+    if (result == ConnectivityResult.wifi) {
+      print('Connected to a Wi-Fi network');
+    } else if (result == ConnectivityResult.mobile) {
+      print('Connected to a mobile network');
+    } else {
+      print('Not connected to any network $result ');
+    }
+
+    setState(() {
+      _connectivityResult = result;
+    });
+  }
+
+  @override
+  initState() {
+    super.initState();
+    // watch the internet connection
+    _connectivitySubscription = Connectivity()
+        .onConnectivityChanged
+        .listen((ConnectivityResult result) {
+      print('Current connectivity status: $result');
+      _connectivityResult = result;
+    });
+  }
+
+  @override
+  dispose() {
+    super.dispose();
+    _connectivitySubscription.cancel();
+  }
+
+  static List<Widget> _widgetOptions = <Widget>[
+    Home(),
+    LiveRadio(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'app',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primaryColor: Color(0xFF2A5592),
-      ),
-      home: Scaffold(
-        body: Home(),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        localizationsDelegates: [
+          GlobalCupertinoLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        supportedLocales: [
+          Locale("ar", "LY"),
+        ],
+        locale: Locale("ar", "LY"),
+        title: 'طريق السلف',
+        theme: ThemeData(
+          primaryColor: Color(0xFF2A5592),
+          // textTheme: GoogleFonts.tajawalTextTheme(
+          //   Theme.of(context).textTheme,
+          // ),
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('طريق السلف'),
+            // elevation: 0,
+            backgroundColor: Color(0xFF2A5089),
+          ),
+          body: Center(
+            child: _widgetOptions.elementAt(_selectedIndex),
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'الرئيسية',
+                backgroundColor: Colors.red,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.radio),
+                label: 'الراديو',
+                backgroundColor: Colors.green,
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            selectedItemColor: Color(0xFF2A5592),
+            onTap: _onItemTapped,
+          ),
+        ));
   }
 }
